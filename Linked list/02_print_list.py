4e4f95158ce2c848c10c92ef6339c8deba0f20da
Aalam <aalamansari1712@gmail.com>
2023-05-21 16:36:47 +0530

    new files added in linked list

---
+++ Linked list/02_print_list.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+my_linkedlist = LinkedList(4)
+
+
+print(my_linkedlist.print_list())
+
+
---
+++ Linked list/03_append_list.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length==0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length+=1
+
+my_linkedlist = LinkedList(4)
+
+my_linkedlist.append(5)
+
+my_linkedlist.print_list()
