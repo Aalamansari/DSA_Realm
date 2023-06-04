43b072a01c1b3fd73d4f264670ba2453c189c54a
Aalam <aalamansari1712@gmail.com>
2023-06-04 18:20:04 +0530

    Doubly Linked List append method added

---
+++ Doubly Linked List/02_DLL_append.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,value):
+        self.value = value 
+        self.next = None
+        self.prev = None
+
+class Doubly_Linked_list:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print(self):
+        temp = self.head
+        while temp!=None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length==None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev =self.tail
+            self.tail = new_node
+        self.length+=1
+        return True
+
+myDLL = Doubly_Linked_list(3)
+myDLL.append(4)
+myDLL.append(5)
+myDLL.print()
