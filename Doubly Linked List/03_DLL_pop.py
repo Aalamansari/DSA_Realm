d459859db9e0c303a004bf7d7122664ff7689df2
Aalam <aalamansari1712@gmail.com>
2023-06-04 23:25:03 +0530

    Doubly Linked List pop method added

---
+++ Doubly Linked List/03_DLL_pop.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,value):
+        self.value = value 
+        self.next = None
+        self.prev = None
+
+class Doubly_Linked_list:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print(self):
+        temp = self.head
+        while temp!=None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.length==None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev =self.tail
+            self.tail = new_node
+        self.length+=1
+        return True
+
+    def pop(self):
+        if self.length==0:
+            return None
+        temp = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        temp.prev = None
+        self.length-=1
+        return temp.value
+
+
+myDLL = Doubly_Linked_list(3)
+myDLL.append(4)
+myDLL.append(5)
+print(myDLL.pop())
