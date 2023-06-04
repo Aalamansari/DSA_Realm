5f84d0c89e196740cbf59108501f549b62c53bb3
Aalam <aalamansari1712@gmail.com>
2023-06-04 18:11:52 +0530

    Doubly Linked List initiated

---
+++ Doubly Linked List/01_doublyLL_constructor.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,value):
+        self.value = value 
+        self.next = None
+        self.prev = None
+
+class Doubly_Linked_list:
+    def __init__(self,value):
+        newnode = Node(value)
+        self.head = value
+        self.tail = value
+        self.length = 1
+
+    def print(self):
+        temp = self.head
+        while temp!=None:
+            print(temp.value)
+            temp = temp.next
+
+    
