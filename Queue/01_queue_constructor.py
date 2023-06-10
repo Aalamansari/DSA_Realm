8d02492edd9aad67282b66845c2ff004221ca1f3
Aalam <aalamansari1712@gmail.com>
2023-06-10 13:00:41 +0530

    Queue constructor added

---
+++ Queue/01_queue_constructor.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length=1
+
+    def print(self):
+        temp = self.first
+        while temp is not None:
+            print(temp)
+            temp = temp.next
