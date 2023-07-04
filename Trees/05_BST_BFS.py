475addaf296b614478a0174c0d435ccef51d09b7
Aalam <aalamansari1712@gmail.com>
2023-07-04 11:02:07 +0530

    checked and update BST insert method

--- Trees/05_BST_BFS.py
+++ Trees/05_BST_BFS.py
@@ -5,13 +5,13 @@ class Node:
         self.right = None
 
 class BinarySearchTree:
+
     def __init__(self):
         self.root = None
 
-
     def insert(self,value):
         new_node = Node(value)
-        if self.root==None:
+        if self.root == None:
             self.root = new_node
             return True
         temp = self.root
@@ -19,7 +19,7 @@ class BinarySearchTree:
             if new_node.value == temp.value:
                 return False
             if new_node.value<temp.value:
-                if temp.left==None: 
+                if temp.left==None:
                     temp.left = new_node
                     return True
                 temp = temp.left
@@ -30,11 +30,9 @@ class BinarySearchTree:
                 temp = temp.right
 
 my_tree = BinarySearchTree()
-my_tree.insert(5)
-my_tree.insert(4)
-my_tree.insert(6)
-
-
+my_tree.insert(12)
+my_tree.insert(11)
+my_tree.insert(14)
 
 print(my_tree.root.value)
 print(my_tree.root.left.value)
